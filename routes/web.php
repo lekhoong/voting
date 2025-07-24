18e5ced7f7bcce6be5fdbad40a9d5676f81421f6
lekhoong <145952727+lekhoong@users.noreply.github.com>
2025-07-24 11:22:23 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\VoteController;
 
-Route::get('/votes', [VoteController::class, 'index'])->name('index');
+Route::get('/', [VoteController::class, 'index'])->name('index');
 Route::post('/votes', [VoteController::class, 'submit'])->name('submit');
 Route::get('/votes/results', [VoteController::class, 'result'])->name('results');
 
